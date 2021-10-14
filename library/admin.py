0049368729de25b5a55213fc1fb1f5f635c0c04b
aanastasiyatuz <aanastasiyatuz@gmail.com>
2021-10-14 09:20:36 +0600

    admin inline

--- library/admin.py
+++ library/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from .models import Book, Order
+from .models import Book, Order, Rating, Comment
 
+    
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+class RatingInline(admin.TabularInline):
+    model = Rating
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'book_id', 'is_available']
     list_filter = ['title', "is_available"]
+    inlines = [CommentInline, RatingInline]
 
 admin.site.register(Book, BookAdmin)
 
