b977c471852c4893e94d906e75d27315e8518453
aanastasiyatuz <aanastasiyatuz@gmail.com>
2021-10-17 16:46:08 +0600

    index for server

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import index
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path('library/', include('library.urls')),
     path('account/', include('account.urls')),
