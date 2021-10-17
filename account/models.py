6d3c775ee52c08a796872c6d6b923eaa260251d4
aanastasiyatuz <aanastasiyatuz@gmail.com>
2021-10-17 21:44:38 +0600

    add email

--- account/models.py
+++ account/models.py
@@ -7,19 +7,23 @@ from phonenumber_field.modelfields import PhoneNumberField
 class MyUserManager(BaseUserManager):
     use_in_migrations = True
 
-    def create_user(self, username, last_name, group, phone, password, **extra_fields):
+    def create_user(self, email, username, last_name, group, phone, password, **extra_fields):
+        if not email: raise ValueError('email is required')
         if not username: raise ValueError('username is required')
         if not last_name: raise ValueError('last_name is required')
         if not group: raise ValueError('group is required')
         if not phone: raise ValueError('phone is required')
 
-        user = self.model(username=username,last_name=last_name,group=group,phone=phone, **extra_fields)
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, last_name=last_name, group=group, phone=phone, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, password, **extra_fields):
-        user = self.model(**extra_fields)
+    def create_superuser(self, email, password, **extra_fields):
+        if not email: raise ValueError('Email is required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)       
         user.set_password(password)
         user.is_superuser = True
         user.is_staff = True
@@ -28,13 +32,14 @@ class MyUserManager(BaseUserManager):
 
 
 class MyUser(AbstractUser):
-    username = models.CharField(max_length=155, unique=True)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=155)
     last_name = models.CharField(max_length=155)
     group = models.CharField(max_length=50)
     phone = PhoneNumberField()
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['group']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['usename', 'last_name', 'group']
 
     objects = MyUserManager()
 
