3116fc3c3b9c086d5047ce2cefdc0211e4a1a36d
aanastasiyatuz <aanastasiyatuz@gmail.com>
2021-10-27 09:47:52 +0600

    book

--- library/views.py
+++ library/views.py
@@ -54,39 +54,42 @@ def BookDetail(request, id):
 
     # all comments
     comments = Comment.objects.filter(book=book)
-    my_rating_list = Rating.objects.filter(book=book, student=request.user)
-    my_rating = my_rating_list[0] if my_rating_list else None
 
-    if request.method == "POST":
-        if len(request.POST) == 1:
-            Order.objects.create(student=request.user, book=book)
-            book.is_available = False
-            book.save()
-            return redirect(reverse_lazy('book-detail', kwargs={"id":id}))
+    if isinstance(request.user, User):
+        my_rating_list = Rating.objects.filter(book=book, student=request.user)
+        my_rating = my_rating_list[0] if my_rating_list else None
 
-        if request.POST.get("body"):
-            # add comment
-            comment_form = CommentForm(request.POST)
-            if comment_form.is_valid():
-                comment_form.save(request, book)
+        if request.method == "POST":
+            if len(request.POST) == 1:
+                Order.objects.create(student=request.user, book=book)
+                book.is_available = False
+                book.save()
                 return redirect(reverse_lazy('book-detail', kwargs={"id":id}))
 
-        elif request.POST.get("rating"):          
-            # add rating
-            rating_form = RatingForm(request.POST)
-            if rating_form.is_valid():
-                rating_form.save(request, book)
-                return redirect(reverse_lazy('book-detail', kwargs={"id":id}))
+            if request.POST.get("body"):
+                # add comment
+                comment_form = CommentForm(request.POST)
+                if comment_form.is_valid():
+                    comment_form.save(request, book)
+                    return redirect(reverse_lazy('book-detail', kwargs={"id":id}))
 
-    else:
-        comment_form = CommentForm()
-        rating_form = RatingForm()
+            elif request.POST.get("rating"):          
+                # add rating
+                rating_form = RatingForm(request.POST)
+                if rating_form.is_valid():
+                    rating_form.save(request, book)
+                    return redirect(reverse_lazy('book-detail', kwargs={"id":id}))
 
-    return render(request, 'book-detail.html', {'book':book, 
-                                                'comment_form':comment_form, 
-                                                'comments':comments, 
-                                                'rating_form':rating_form,
-                                                'my_rating':my_rating})
+        else:
+            comment_form = CommentForm()
+            rating_form = RatingForm()
+
+        return render(request, 'book-detail.html', {'book':book, 
+                                                    'comment_form':comment_form, 
+                                                    'comments':comments, 
+                                                    'rating_form':rating_form,
+                                                    'my_rating':my_rating})
+    return render(request, "book-detail.html", {"book":book, "comments":comments})
 
 
 """--------CREATE---------"""
